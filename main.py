0a710a47278e60b12ea955a8b99c9d549972236f
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2022-09-30 10:46:13 +0700

    feat: cli implement

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ import random
 import utils
 import json
 from pathlib import Path
+import argparse
+import sys
 
-BG_PATH = "train/backgrounds"
-ID_CARD_PATH = "train/id-cards"
-OUTPUT_IMAGE_PATH = "export/data_sample"
-OUTPUT_MASK_PATH = "export/ground_truth"
-GROUND_TRUTH_DIR = "ground_truth"
+BG_PATH = 'train/backgrounds'
+ID_CARD_PATH = 'train/id-cards'
+OUTPUT_IMAGE_PATH = 'export/data_sample'
+OUTPUT_MASK_PATH = 'export/ground_truth'
+GROUND_TRUTH_DIR = 'ground_truth'
 CARD_SAMPLE_WIDTH = 1280
 CARD_SAMPLE_HEIGHT = 814
-DATASET_PREFIX = ["DG", "DX", "LG", "LX"]
+DATASET_PREFIX = ['DG', 'DX', 'LG', 'LX']
 VERSION = 31
 COUNT_ITEM = 30
 DELTA = 15
@@ -21,24 +23,24 @@ SCALE_MAX = 60
 
 
 def load_bg_img(bg_name: str) -> Image.Image:
-    """Load background image using file name
+    '''Load background image using file name
     Args:
         background_name (str): background image name
-    """
+    '''
 
-    bg_path = path.join(BG_PATH, f"{bg_name}.jpeg")
+    bg_path = path.join(BG_PATH, f'{bg_name}.jpeg')
     bg_img = Image.open(bg_path)
 
     return bg_img
 
 
 def load_obj_img(obj_name: str) -> Image.Image:
-    """Load id card image using file name
+    '''Load id card image using file name
     Args:
        id_card_name (str): id card image name
-    """
+    '''
 
-    obj_img_path = path.join(ID_CARD_PATH, f"{obj_name}.png")
+    obj_img_path = path.join(ID_CARD_PATH, f'{obj_name}.png')
     obj_img = Image.open(obj_img_path)
     return obj_img
 
@@ -62,17 +64,20 @@ def show_img(img: Image.Image) -> None:
 
 
 def create_color_bg(bg_size: tuple[int, int], bg_color: tuple[int, int, int] = (0, 0, 0)) -> Image.Image:
-    bg_with_color = Image.new("RGB", size=bg_size, color=bg_color)
+    bg_with_color = Image.new('RGB', size=bg_size, color=bg_color)
     return bg_with_color
 
 
 def choose_new_obj_size(bg_size: tuple[int, int], orig_obj_size: tuple[int, int]) -> tuple[int, int]:
-    bg_width, _ = bg_size
+    bg_width, bg_height = bg_size
     org_obj_width, org_obj_height = orig_obj_size
     scale_ratio = random.randint(SCALE_MIN, SCALE_MAX) / 100
     new_width = int(scale_ratio * bg_width)
     new_height = int((new_width / org_obj_width) * org_obj_height)
-
+    # in case new height > background height
+    if new_height > bg_height:
+        new_height = bg_height - 5  # extra padding
+        new_width = int((new_height / org_obj_height) * org_obj_width)
     return (new_width, new_height)
 
 
@@ -103,7 +108,7 @@ def random_transform_perspective(img: Image.Image) -> Image.Image:
     return new_img
 
 
-def generate_synthetic_img(bg_img: Image.Image, obj_img: Image.Image, output_name: str):
+def generate_synthetic_img(bg_img: Image.Image, obj_img: Image.Image, output_name: str, out_dir: str):
     bg_img_size = bg_img.size
 
     bg_resized = resize_img(img=bg_img, new_size=bg_img_size)
@@ -133,62 +138,73 @@ def generate_synthetic_img(bg_img: Image.Image, obj_img: Image.Image, output_nam
     bg_resized.paste(obj_resized, coordinates, mask=obj_resized)
     result_bg.paste(result_mask, coordinates, mask=result_mask)
 
-    output_generated_image_path = path.join(
-        OUTPUT_IMAGE_PATH, f"{output_name}.png")
-    output_generated_mask = path.join(
-        OUTPUT_MASK_PATH, f"{output_name}.png")
-
-    bg_resized.save(output_generated_image_path)
-    result_bg.save(output_generated_mask)
+    bg_resized.save(f"{out_dir}/data_sample/{output_name}.png")
+    result_bg.save(f"{out_dir}/ground_truth/{output_name}.png")
 
 
 def get_ground_truth_quad(file_name: str):
-    json_file_path = path.join(GROUND_TRUTH_DIR, f"{file_name}.json")
+    json_file_path = path.join(GROUND_TRUTH_DIR, f'{file_name}.json')
     json_file = open(json_file_path)
     data = json.load(json_file)
     xy = []
-    for xy_item in data["quad"]:
+    for xy_item in data['quad']:
         xy.append(tuple(xy_item))
     json_file.close()
     return xy
 
 
 def create_data_sample(prefix: str, file_name: str):
-    orig_img = Image.open(f"import/{prefix}/{file_name}.tif")
+    orig_img = Image.open(f'import/{prefix}/{file_name}.tif')
     bg_img = create_color_bg(bg_size=orig_img.size)
-    xy = get_ground_truth_quad(f"{prefix}/{file_name}")
+    xy = get_ground_truth_quad(f'{prefix}/{file_name}')
     d = ImageDraw.Draw(bg_img)
     d.polygon(xy, fill=(255, 255, 255))
-    orig_img.save(f"export/data_sample/{file_name}.png")
-    bg_img.save(f"export/ground_truth/{file_name}.png")
+    orig_img.save(f'export/data_sample/{file_name}.png')
+    bg_img.save(f'export/ground_truth/{file_name}.png')
 
 
 def load_dataset():
     for prefix in DATASET_PREFIX:
         for i in range(1, COUNT_ITEM + 1):
-            print(f"Loading data sample {i} ...")
             create_data_sample(
-                prefix=prefix, file_name=f"{prefix}{VERSION}_{i:02d}")
+                prefix=prefix, file_name=f'{prefix}{VERSION}_{i:02d}')
 
 
-def generate_bulk_data(num_data: int, bg_path: str, output_prefix: str, obj_path: str):
-    bg_images = [str(p) for p in Path(bg_path).glob("*.png")]
-    obj_images = [str(p) for p in Path(obj_path).glob("*.png")]
+def generate_bulk_data(num_data: int, bg_path: str, output_prefix: str, obj_path: str, out_dir: str):
+    bg_images = [str(p) for p in Path(bg_path).glob('*.png')]
+    obj_images = [str(p) for p in Path(obj_path).glob('*.png')]
 
     for i in range(1, num_data + 1):
-        print(f"Generating data {i:02d} ...")
+        print(f'Generating data {i:02d} ...')
         random_bg = random.choice(bg_images)
         random_obj = random.choice(obj_images)
         random_bg_img = Image.open(random_bg)
         random_obj_img = Image.open(random_obj)
         generate_synthetic_img(
-            bg_img=random_bg_img, obj_img=random_obj_img, output_name=f"{output_prefix}_{i:02d}")
+            bg_img=random_bg_img, obj_img=random_obj_img, output_name=f'{output_prefix}_{i:02d}', out_dir=out_dir)
 
 
-# bg_img = Image.open("train/backgrounds/table/table_07.png")
-# obj_img = Image.open("train/jpn_driver_license/jpn_license_01.png")
-# bg_images = [str(p) for p in Path("train/backgrounds/table").glob("*.png")]
-# print(str(random.choice(bg_images)))
+def main():
 
-generate_bulk_data(num_data=5, bg_path="train/backgrounds/table",
-                   obj_path="train/jpn_driver_license", output_prefix="TABLE")
+    parser = argparse.ArgumentParser(
+        description='Generate synthetic data from background and object images')
+    parser.add_argument('--num', type=int, metavar='N', default=10,
+                        help='the number of synthetic images')
+    parser.add_argument('--in-bg', metavar='BG', required=True,
+                        help='input background directory path')
+    parser.add_argument('--in-obj', metavar='OBJ', required=True,
+                        help='input object directory path')
+    parser.add_argument('--img-prefix', metavar='PREFIX', required=True,
+                        help='output images name prefix')
+    parser.add_argument('--out-dir', type=str, metavar='OUTPUT', default='export',
+                        help='output directory path')
+    args = parser.parse_args()
+    if not len(sys.argv) > 1:
+        parser.print_help()
+    else:
+        generate_bulk_data(num_data=args.num, bg_path=args.in_bg,
+                           obj_path=args.in_obj, output_prefix=args.img_prefix, out_dir=args.out_dir)
+
+
+if __name__ == '__main__':
+    main()
